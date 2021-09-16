b3c3f3e75e452b9b648e82028dd5a840ca6cd233
Aliaxander Novikov <nerevarin11@gmail.com>
2021-09-16 14:47:04 +0300

    propTypes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './Car.css'
 import withClass from '../hoc/withClass'
+import PropTypes from 'prop-types'
 
 class Car extends React.Component {
   render() {
@@ -31,6 +32,13 @@ class Car extends React.Component {
     </React.Fragment>
   )
   }
+}
+
+Car.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.number,
+  onChangeName: PropTypes.func,
+  onDelete: PropTypes.func
 }
 
 export default withClass(Car, classes.Car);
